845be02a01b3b181c91846505fdd014d8539e0ab
Dani khalang <danikhalang@gmail.com>
2025-09-23 18:15:58 +0200

    main container of the game

---
+++ pixi-react-game/src/components/Experience/MainContainer/MainContainer.tsx
@@ -0,0 +1,27 @@
+import { Container, Sprite } from "@pixi/react";
+import { Texture } from "@pixi/core";
+import { useMemo, type PropsWithChildren } from "react";
+import backgroundAsset from '../../../assets/Piazza-Vittorio.webp'
+import { Level } from "../../Levels/Level";
+
+interface IMainContainerProps {
+    canvasSize: { width: number; height: number };
+}
+
+export const MainContainer = ({ canvasSize, children }: PropsWithChildren<IMainContainerProps>) => {
+
+    const backgroundTexture = useMemo(() => Texture.from(backgroundAsset), [])
+
+    return (
+        <Container>
+            <Sprite
+                texture={backgroundTexture}
+                width={canvasSize.width}
+                height={canvasSize.height}
+                
+            />
+            {children}
+            <Level />
+        </Container>
+    );
+}
