14c6ff3963e0d1c8d2e81de045b93a780d04da2a
Dani khalang <danikhalang@gmail.com>
2025-09-23 18:18:48 +0200

    pixi and react version downgraded

--- pixi-react-game/src/App.tsx
+++ pixi-react-game/src/App.tsx
@@ -1,17 +1,11 @@
-import { useState, useRef } from 'react'
-import { Application } from '@pixi/react'
+import { Experience } from "./components/Experience/Experience"
 
 
-function App() {
-  const parentRef = useRef(null);
-
+const App = () => {
   return (
-    <div ref={parentRef}>
-      <Application resizeTo={parentRef} />
-      <pixiContainer x={100} y={100}>
-      <pixiSprite anchor={{ x: 0.5, y: 0.5 }} texture={texture} />
-      </pixiContainer>  
-    </div>
+    <>
+      <Experience />
+    </>
   )
 }
 
